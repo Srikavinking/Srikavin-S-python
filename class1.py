dafe9f6996e07796a269fe93a77835546354511c
Srikavinking <srikavinmtrs2009@gmail.com>
2026-04-29 16:41:14 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,29 @@
+print("Hellow world")
+a=20
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+print("to find square root of 4")
+z=4
+ans=z**0.5
+print('square root of an num 4',ans)
+
+base=10
+height=5
+area=0.5*base*height
+print("area of the triangle is:",area)
+
+a=30
+b=18
+print('(a+b)^2=',(a+b)**2)
+print('(a-b)^2=',(a-b)**2)
+print('a^2-b^2=',a**2-b**2)
+
+
+
+
+
