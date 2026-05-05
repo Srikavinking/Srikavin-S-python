ed994dbfd689c903d0b0721be3b6e5e0e91c8c52
Srikavinking <srikavinmtrs2009@gmail.com>
2026-05-05 15:56:54 +0530

    Add files via upload

---
+++ class5.py
@@ -0,0 +1,32 @@
+#1
+'''a= "king of pidaripatti"
+print('a')
+print(a.count(" ")+1)
+
+#2
+s=str(input("Enter a string"))
+print('uppercase',s.upper())
+print('lowercase',s.lower())
+
+#3
+c=str(input("enter a sentanse="))
+no_space=c.replace(" ","")
+print("without the spacing:",no_space)
+
+#4
+d=str(input("enter a sentanse="))
+count=0
+for i in d:
+    count+=1
+    print("length of string:",count)
+
+#5
+e=str(input("enter a sentanse="))
+f=str(input("enter a letter to count")) 
+print(e)
+print(e.count(f))'''
+
+#6
+g=str(input("enter a string = "))
+for i in g:
+    print(i)
