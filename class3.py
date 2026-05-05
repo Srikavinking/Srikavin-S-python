476298d925b4643f344aafdd64b13ef164e7279b
Srikavinking <srikavinmtrs2009@gmail.com>
2026-05-05 15:51:39 +0530

    Add files via upload

---
+++ class3.py
@@ -0,0 +1,54 @@
+#1
+for i in range(1,11):
+    print(i)
+
+#2
+for i in range(2,52,2):
+    print(i)
+
+#3
+for i in range(1,51,2):
+    print(i)
+
+#4
+num=int(input("Enter the number"))
+for i in range(1,11):
+    print(num,"x",i,"=",num*i)
+
+#5
+Total=0
+for i in range(1,101):
+    Total += i
+    print('sum',Total)
+
+#6
+for i in range(10,0,-1):
+    print(i)
+
+#7
+Count=0
+for i in range(1,101):
+    if i % 7 == 0:
+        Count+= 1
+        print("Count",Count)
+
+#8
+for i in range(1,101):
+    if i % 3==0 and i % 5!=0:
+        print(i)
+
+#9
+num=int(input("Enter the number"))
+fact=1
+for i in range(1,num,1):
+    fact*=i
+print('Factorial',fact)
+
+#10
+
+        
+
+    
+    
+
+    
