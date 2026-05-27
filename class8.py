f27a3712c9601ff827eefd8809187e232d7af7ff
Srikavinking <srikavinmtrs2009@gmail.com>
2026-05-27 16:04:27 +0530

    Add files via upload

---
+++ class8.py
@@ -0,0 +1,84 @@
+'''#Core Data type
+
+#Tuple
+a=(1,2,5,13)
+print(type(a))
+
+#List
+a=[1,2,5,13]
+print(type(a))
+
+#Set
+a={1,2,5,13}
+print(type(a))
+
+k=[1,2,3,7,5,6]
+k.append(23)
+print(k)
+
+a=[1,2,3,1,4,5,6,1]
+
+print(a)
+# add an element into last
+a.append(56)
+print(a)
+# duplicate list
+p=a.copy()      
+print(p)
+print(a.count(1))
+
+# combine of two list
+print(p.extend(a))   
+print(a)
+
+# store
+print(p)       
+print(p.index(1))
+
+# index value element insert
+print(a.insert(7,234))   
+print(a)
+
+# last element delete
+print(a.pop())   
+print(a)
+
+# index element remove
+print(a.pop(1))   
+print(a)
+
+# element remove
+print(a.remove(3))   
+print(a)
+
+print(a.reverse())
+print(a)
+
+print(a.sort())
+print(a)'''
+
+
+n=str(input('Enter the name'))
+k=str("king of pidaripatti")
+i=("is")
+I=("I am right Master Dr.Srikavin
+print(n,i,k)
+print(k,i,n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
