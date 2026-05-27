b0f90a06eac1d2cacaa732175e0d12a3b22becf9
Srikavinking <srikavinmtrs2009@gmail.com>
2026-05-27 16:04:02 +0530

    Add files via upload

---
+++ class7 p.py
@@ -0,0 +1,40 @@
+'''for row in range(1,7):
+    for col in range(row):
+        print('*',end=" ")
+        print('\n')'''
+
+'''for k in range(1,8,1):
+    for j in range(1,7,1):
+        if(k==1 or k==4 or k==7):
+            print("$",end=" ")
+        elif k in[2,3] and j==1:
+            print("$",end=" ")
+        elif k in[5,6] and j==6:
+            print("$",end=" ")
+        else:
+            print("$",end=" ")
+            print()
+
+            
+for i in range (1,7,1):
+    for j in range(5,i-1,-1):
+        print(" ",end="")
+    print(i*"* ",end='')
+    print()
+
+for i in range (1,7,1):
+    for j in range(5,i-1,-1):
+        print(end="")
+    print(i*"* ",end='')
+    print()'''
+
+for i in range (1,7,1):
+    for j in range(5,i+1,1):
+        print(" ",end="")
+    print(i*"*",end='')
+    print()
+
+
+
+
+
