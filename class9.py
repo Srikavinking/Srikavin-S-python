4aa0b120f26554c138d0cb8f4be0b1affd03853f
Srikavinking <srikavinmtrs2009@gmail.com>
2026-05-27 16:23:32 +0530

    Add files via upload

---
+++ class9.py
@@ -0,0 +1,73 @@
+'''a={1,2,3,4,5}
+b={1,5,6,7,2}
+print(a,b)
+a.difference(b)
+print(a)
+print(b)
+#without Argument
+def fun(add):
+    (a,b)
+a=12
+b=10
+c=a+b
+print(c)
+
+def fun(sub):
+    (a,b)
+a=12
+b=10
+c=a-b
+print(c)
+
+#with Argument
+
+def add(a,b):
+    c=a+b    
+print(c)
+add(12,10)
+
+def sub(a,b):
+    c=a-b
+print(c)
+add(12,10)'''
+
+#Defult Argument
+def function(a,b,c):
+    print("addition is:",a+b+c)
+    print("subtraction:",a-b)
+    function(30)
+    function(50,30)
+    function(50,30,10)
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
